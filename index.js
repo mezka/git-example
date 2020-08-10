f9845aae10e55ce2955a2c59575bf503ef9590c6
Emiliano Mesquita <emiliano_mesquita@hotmail.com>
2020-08-10 09:28:11 -0300

    Change text to Hola Mundo

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var figlet = require('figlet');
  
-figlet('Hello World!!', function(err, data) {
+figlet('Hola mundo!!', function(err, data) {
     if (err) {
         console.log('Something went wrong...');
         console.dir(err);
